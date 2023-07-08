ebbd9ffda495689ccbbcfc2de595de95561965c2
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-07-08 12:19:32 +0200

    rename apps

--- apps/web_shell/src/index.tsx
+++ apps/web_shell/src/index.tsx
@@ -1,1 +1,9 @@
-import("./bootstrap");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+
+const Hello = () => <h1>Hello from React!</h1>;
+
+root.render(<Hello />);
