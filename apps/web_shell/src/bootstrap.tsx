059621d7e7c28b5d2696ea29c41a12ad653d3545
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-07-08 11:45:30 +0200

    handle running on dev and prod

--- apps/web_shell/src/bootstrap.tsx
+++ apps/web_shell/src/bootstrap.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const container = document.getElementById("root")!;
-const root = createRoot(container);
-
 const Hello = () => <h1>Hello from React!</h1>;
 
-root.render(<Hello />);
+const mount = (el:string) => {
+  const container = document.getElementById(el)!;
+  const root = createRoot(container);
+
+  root.render(<Hello />);
+}
+
+if (process.env.NODE_ENV === 'development') {
+  const container = document.getElementById('root')!;
+
+  if (container) {
+    mount('root')
+  }
+}
+
+export {mount}
