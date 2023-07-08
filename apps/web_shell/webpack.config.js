27aa4fc3b6619933815c800d4f3baf8e94909e08
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-07-08 10:27:19 +0200

    add htmlwebpack plugin

--- apps/web_shell/webpack.config.js
+++ apps/web_shell/webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
+  mode: 'production',
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "build"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
 };
