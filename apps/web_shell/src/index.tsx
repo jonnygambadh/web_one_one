558004560316aa3e80110c625284bf1d3184d351
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-07-08 16:42:02 +0200

    add sentry

--- apps/web_shell/src/index.tsx
+++ apps/web_shell/src/index.tsx
@@ -1,1 +1,15 @@
+import * as Sentry from "@sentry/react";
+
 import("./bootstrap");
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+    }),
+  ],
+
+  tracesSampleRate: 1.0,
+});
--- apps/web_shell/webpack.common.js
+++ apps/web_shell/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: "./src/index.tsx",
@@ -20,6 +21,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
     }),
