d138ba92e3e947ff519db7ebba1b73a9ca5197e3
Jonny Gamba <jonny.gamba@deliveryhero.com>
2023-07-08 12:35:54 +0200

    fix names

--- apps/web_shell/src/bootstrap.tsx
+++ apps/web_shell/src/bootstrap.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const Hello = () => <h1>Hello from React!</h1>;
+const Hello = () => <h1>Hello from React - Shell!</h1>;
 
-  const container = document.getElementById(el)!;
-  const root = createRoot(container);
+const container = document.getElementById("root")!;
+const root = createRoot(container);
 
-  root.render(<Hello />);
-
+root.render(<Hello />);
